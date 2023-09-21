9303c4b357fd7970e5403f7b407193c818e54797
xiquan <xi-quan@163.com>
2023-09-21 10:39:47 +0800

    update spmp register operation APIs in spmp.c

--- lib/sbi/sm/platform/spmp/spmp.c
+++ lib/sbi/sm/platform/spmp/spmp.c
@@ -1,5 +1,4 @@
-#include <sm/platform/spmp/spmp.h>
-//#include <sm/ipi.h>
+#include <sm/platform/spmp_xs/spmp.h>
 #include <stddef.h>
 
 void set_spmp(int spmp_idx, struct spmp_config_t spmp_cfg_t)
@@ -7,7 +6,7 @@ void set_spmp(int spmp_idx, struct spmp_config_t spmp_cfg_t)
 #if 1
   uintptr_t spmp_address = 0;
   uintptr_t old_config = 0;
-  uintptr_t spmp_config = ((spmp_cfg_t.mode & SPMP_A) | (spmp_cfg_t.perm & (SPMP_R|SPMP_W|SPMP_X)))
+  uintptr_t spmp_config = ((spmp_cfg_t.sbit & SPMP_S) | (spmp_cfg_t.mode & SPMP_A) | (spmp_cfg_t.perm & (SPMP_R|SPMP_W|SPMP_X)))
     << ((uintptr_t)SPMPCFG_BIT_NUM * (spmp_idx % SPMP_PER_CFG_REG));
 
   switch(spmp_cfg_t.mode)
@@ -30,6 +29,9 @@ void set_spmp(int spmp_idx, struct spmp_config_t spmp_cfg_t)
 
   switch(spmp_idx)
   {
+#define X(n, g) case n: { SPMP_SET(n, g, spmp_addr, spmp_config); break; }
+//  LIST_OF_SPMP_REGS
+#undef X
     case 0:
       old_config = read_spmpcfg(spmpcfg0);
       spmp_config |= (old_config &
@@ -41,43 +43,91 @@ void set_spmp(int spmp_idx, struct spmp_config_t spmp_cfg_t)
       spmp_config |= (old_config &
           ~((uintptr_t)SPMPCFG_BITS << (uintptr_t)SPMPCFG_BIT_NUM*(1%SPMP_PER_CFG_REG)));
       SPMP_SET(spmpaddr1, spmpcfg0, spmp_address, spmp_config);
-     break;
+      break;
     case 2:
       old_config = read_spmpcfg(spmpcfg0);
       spmp_config |= (old_config &
           ~((uintptr_t)SPMPCFG_BITS << (uintptr_t)SPMPCFG_BIT_NUM*(2%SPMP_PER_CFG_REG)));
       SPMP_SET(spmpaddr2, spmpcfg0, spmp_address, spmp_config);
-     break;
+      break;
     case 3:
       old_config = read_spmpcfg(spmpcfg0);
       spmp_config |= (old_config &
           ~((uintptr_t)SPMPCFG_BITS << (uintptr_t)SPMPCFG_BIT_NUM*(3%SPMP_PER_CFG_REG)));
       SPMP_SET(spmpaddr3, spmpcfg0, spmp_address, spmp_config);
-     break;
+      break;
     case 4:
       old_config = read_spmpcfg(spmpcfg0);
       spmp_config |= (old_config &
           ~((uintptr_t)SPMPCFG_BITS << (uintptr_t)SPMPCFG_BIT_NUM*(4%SPMP_PER_CFG_REG)));
       SPMP_SET(spmpaddr4, spmpcfg0, spmp_address, spmp_config);
-     break;
+      break;
     case 5:
       old_config = read_spmpcfg(spmpcfg0);
       spmp_config |= (old_config &
           ~((uintptr_t)SPMPCFG_BITS << (uintptr_t)SPMPCFG_BIT_NUM*(5%SPMP_PER_CFG_REG)));
       SPMP_SET(spmpaddr5, spmpcfg0, spmp_address, spmp_config);
-     break;
+      break;
     case 6:
       old_config = read_spmpcfg(spmpcfg0);
       spmp_config |= (old_config &
           ~((uintptr_t)SPMPCFG_BITS << (uintptr_t)SPMPCFG_BIT_NUM*(6%SPMP_PER_CFG_REG)));
       SPMP_SET(spmpaddr6, spmpcfg0, spmp_address, spmp_config);
-     break;
+      break;
     case 7:
       old_config = read_spmpcfg(spmpcfg0);
       spmp_config |= (old_config &
           ~((uintptr_t)SPMPCFG_BITS << (uintptr_t)SPMPCFG_BIT_NUM*(7%SPMP_PER_CFG_REG)));
       SPMP_SET(spmpaddr7, spmpcfg0, spmp_address, spmp_config);
-     break;
+      break;
+    case 8:
+      old_config = read_spmpcfg(spmpcfg2);
+      spmp_config |= (old_config &
+          ~((uintptr_t)SPMPCFG_BITS << (uintptr_t)SPMPCFG_BIT_NUM*(8%SPMP_PER_CFG_REG)));
+      SPMP_SET(spmpaddr8, spmpcfg2, spmp_address, spmp_config);
+      break;
+    case 9:
+      old_config = read_spmpcfg(spmpcfg2);
+      spmp_config |= (old_config &
+          ~((uintptr_t)SPMPCFG_BITS << (uintptr_t)SPMPCFG_BIT_NUM*(9%SPMP_PER_CFG_REG)));
+      SPMP_SET(spmpaddr9, spmpcfg2, spmp_address, spmp_config);
+      break;
+    case 10:
+      old_config = read_spmpcfg(spmpcfg2);
+      spmp_config |= (old_config &
+          ~((uintptr_t)SPMPCFG_BITS << (uintptr_t)SPMPCFG_BIT_NUM*(10%SPMP_PER_CFG_REG)));
+      SPMP_SET(spmpaddr10, spmpcfg2, spmp_address, spmp_config);
+      break;
+    case 11:
+      old_config = read_spmpcfg(spmpcfg2);
+      spmp_config |= (old_config &
+          ~((uintptr_t)SPMPCFG_BITS << (uintptr_t)SPMPCFG_BIT_NUM*(11%SPMP_PER_CFG_REG)));
+      SPMP_SET(spmpaddr11, spmpcfg2, spmp_address, spmp_config);
+      break;
+    case 12:
+      old_config = read_spmpcfg(spmpcfg2);
+      spmp_config |= (old_config &
+          ~((uintptr_t)SPMPCFG_BITS << (uintptr_t)SPMPCFG_BIT_NUM*(12%SPMP_PER_CFG_REG)));
+      SPMP_SET(spmpaddr12, spmpcfg2, spmp_address, spmp_config);
+      break;
+    case 13:
+      old_config = read_spmpcfg(spmpcfg2);
+      spmp_config |= (old_config &
+          ~((uintptr_t)SPMPCFG_BITS << (uintptr_t)SPMPCFG_BIT_NUM*(13%SPMP_PER_CFG_REG)));
+      SPMP_SET(spmpaddr13, spmpcfg2, spmp_address, spmp_config);
+      break;
+    case 14:
+      old_config = read_spmpcfg(spmpcfg2);
+      spmp_config |= (old_config &
+          ~((uintptr_t)SPMPCFG_BITS << (uintptr_t)SPMPCFG_BIT_NUM*(14%SPMP_PER_CFG_REG)));
+      SPMP_SET(spmpaddr14, spmpcfg2, spmp_address, spmp_config);
+      break;
+    case 15:
+      old_config = read_spmpcfg(spmpcfg2);
+      spmp_config |= (old_config &
+          ~((uintptr_t)SPMPCFG_BITS << (uintptr_t)SPMPCFG_BIT_NUM*(15%SPMP_PER_CFG_REG)));
+      SPMP_SET(spmpaddr15, spmpcfg2, spmp_address, spmp_config);
+      break;
     default:
       break;
   }
@@ -133,6 +183,30 @@ struct spmp_config_t get_spmp(int spmp_idx)
       break;
     case 7:
       SPMP_READ(spmpaddr7, spmpcfg0, spmp_address, spmp_config);
+      break;
+    case 8:
+      SPMP_READ(spmpaddr8, spmpcfg2, spmp_address, spmp_config);
+      break;
+    case 9:
+      SPMP_READ(spmpaddr9, spmpcfg2, spmp_address, spmp_config);
+      break;
+    case 10:
+      SPMP_READ(spmpaddr10, spmpcfg2, spmp_address, spmp_config);
+      break;
+    case 11:
+      SPMP_READ(spmpaddr11, spmpcfg2, spmp_address, spmp_config);
+      break;
+    case 12:
+      SPMP_READ(spmpaddr12, spmpcfg2, spmp_address, spmp_config);
+      break;
+    case 13:
+      SPMP_READ(spmpaddr13, spmpcfg2, spmp_address, spmp_config);
+      break;
+    case 14:
+      SPMP_READ(spmpaddr14, spmpcfg2, spmp_address, spmp_config);
+      break;
+    case 15:
+      SPMP_READ(spmpaddr15, spmpcfg2, spmp_address, spmp_config);
       break;
     default:
       break;
